5b0b4d224ccb762bd15d070882ec76f1045fae67
ESeokhwan <huan99@hanmail.net>
2025-01-08 03:59:47 +0000

    feat: modulate total num of record

--- scripts/producer_expr_cold.sh
+++ scripts/producer_expr_cold.sh
@@ -2,7 +2,7 @@
 
 # 1차 Java 명령어 실행
 echo "Running Java Kafka producer..."
-java -cp /home/huan99/kafka_test_clients/build/libs/kafka_test_clients-1.0-SNAPSHOT-all.jar org.example.BasicProducerWithMonitor localhost:9092 test_topic_ms_10000 -s 10000 -n 400000 -m output/cur/producer_monitor_cold.csv
+java -cp /home/huan99/kafka_test_clients/build/libs/kafka_test_clients-1.0-SNAPSHOT-all.jar org.example.BasicProducerWithMonitor localhost:9092 test_topic_ms_10000 -s 10000 -n 200000 -m output/cur/producer_monitor_cold.csv
 
 # Produce 완료 메시지
 echo "Producer job done."
